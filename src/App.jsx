826bc555f30b5d26380008a5c2772b27b8bb9669
Nooa Peltokangas <nooa.peltokangas@protonmail.com>
2024-01-18 13:20:45 +0200

    switch dark mode handling to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 import Header from "./components/Header"
 
 function App() {
+
+  if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    document.getElementById('root').classList.add('dark')
+  }
+
   return (
-    <Header />
+    <div className="w-screen h-screen bg-stone-200 dark:bg-stone-800">
+      <Header />
+    </div>
   )
 }
 
