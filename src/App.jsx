ae9bf7a78f1db72180bb588c424a2cc27793ee12
Nooa Peltokangas <nooa.peltokangas@protonmail.com>
2024-01-18 14:38:43 +0200

    add padding, edit font size on small screens

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Header from "./components/Header"
 
 function App() {
   return (
-    <div className="w-screen h-screen bg-stone-200 dark:bg-zinc-900 text-zinc-900 dark:text-stone-200 pt-8 transition-colors duration-75 ease-in">
+    <div className="w-screen h-screen bg-stone-200 dark:bg-zinc-900 text-zinc-900 dark:text-stone-200 pt-8 transition-colors duration-75 ease-in px-4">
       <Header />
       <div id="about" className="">about</div>
       <div id="project">project</div>
