b06b81df5f66f4932f83cebd3584e698b24e5b36
Nooa Peltokangas <nooa.peltokangas@protonmail.com>
2024-01-18 13:38:46 +0200

    simple dark mode toggle

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,6 @@
 import Header from "./components/Header"
 
 function App() {
-
-  if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
-    document.getElementById('root').classList.add('dark')
-  }
-
   return (
     <div className="w-screen h-screen bg-stone-200 dark:bg-stone-800">
       <Header />
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,42 @@
+import { useState, useEffect } from "react"
+import { Moon, Sun } from "lucide-react"
+
+function Toggle() {
+  const [isDark, setIsDark] = useState(false)
+
+  useEffect(() => {
+    if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+      document.getElementById('root').classList.add('dark')
+      setIsDark(true)
+    }
+  }, [])
+
+  function handleClick() {
+    if (isDark) {
+      setIsDark(false)
+      document.getElementById('root').classList.remove('dark')
+    } else {
+      setIsDark(true)
+      document.getElementById('root').classList.add('dark')
+    }
+  }
+
+  if (isDark) {
+    return <Sun onClick={handleClick}/>
+  } else {
+    return <Moon onClick={handleClick}/>
+  }
+}
+
 function Header() {
   return (
-    <header>
+    <header className="flex flex-row">
       <h1>Nooa Peltokangas</h1>
       <nav>
-
+        <a href="/">About</a>
+        <a href="/">Projects</a>
+        <a href="/">Contact</a>
+        <Toggle />
       </nav>
     </header>
   )
