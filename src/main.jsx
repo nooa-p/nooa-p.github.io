a27eadc34998d131d29566e19f0d6f8a1dca4270
Nooa Peltokangas <nooa.peltokangas@protonmail.com>
2024-01-18 13:13:17 +0200

    page checks for dark mode user preference

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+  document.getElementById('root').classList.add('dark')
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
