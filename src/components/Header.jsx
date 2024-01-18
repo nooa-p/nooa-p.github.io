f7e964d0a76f05ebdc8e04544a5d0795dbe37202
Nooa Peltokangas <nooa.peltokangas@protonmail.com>
2024-01-18 14:36:36 +0200

    edit font weights

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -22,7 +22,7 @@ function Toggle() {
   }
 
   return (
-    <div onClick={handleClick} className="hover:cursor-pointer">
+    <div onClick={handleClick} className="hover:cursor-pointer self-center">
       <Moon className={isDark ? 'opacity-0' : 'opacity-100'} />
       <Sun className={'-mt-6 ' + (isDark ? 'opacity-100' : 'opacity-0')} />
     </div>
@@ -31,9 +31,9 @@ function Toggle() {
 
 function Header() {
   return (
-    <header className="flex flex-row justify-between max-w-3xl mx-auto lowercase">
-      <h1 className="hover:cursor-default">Nooa P.</h1>
-      <nav className="flex flex-row gap-x-4 md:gap-x-8">
+    <header className="flex flex-row justify-between max-w-3xl mx-auto lowercase text-lg">
+      <h1 className="hover:cursor-default font-extrabold">Nooa P.</h1>
+      <nav className="flex flex-row gap-x-4 md:gap-x-8 font-semibold">
         <a href="#about">About</a>
         <a href="#project">Projects</a>
         <a href="#contact">Contact</a>
