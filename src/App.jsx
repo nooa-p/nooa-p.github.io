9f4be319721bad23cebe4ac04d70c059aed284c4
Nooa Peltokangas <nooa.peltokangas@protonmail.com>
2024-02-16 12:22:03 +0200

    add gradient colors to tailwind theme

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Footer from "./components/Footer"
 
 function App() {
   return (
-    <main className="max-w-3xl mx-auto mt-12 flex flex-col p-8">
+    <main className="max-w-3xl mx-auto mt-12 flex flex-col p-8 gap-4">
       <Header />
         <Intro />
         <About />
