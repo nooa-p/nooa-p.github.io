487cfd528584a92a58a1865cbf45bc92277bac5b
Nooa Peltokangas <nooa.peltokangas@protonmail.com>
2024-02-20 13:35:24 +0200

    resize copyright symbol

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import { Copyright } from "lucide-react"
 function Footer() {
   return (
     <div className="flex flex-row text-xs justify-center items-center gap-1.5 font-light">
-      <Copyright size={16} strokeWidth={0.75} absoluteStrokeWidth />
+      <Copyright size={14} strokeWidth={0.75} absoluteStrokeWidth />
       <p>Nooa Peltokangas</p>
     </div>
   )
