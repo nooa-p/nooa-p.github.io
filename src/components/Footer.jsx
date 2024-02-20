8ecc458f88e48b9e44aa427cdd009bb19c568e2a
Nooa Peltokangas <nooa.peltokangas@protonmail.com>
2024-02-20 12:54:04 +0200

    edit footer font weight

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,8 @@ import { Copyright } from "lucide-react"
 
 function Footer() {
   return (
-    <div className="flex flex-row text-xs justify-center items-center gap-1.5">
-      <Copyright size={14} strokeWidth={1} absoluteStrokeWidth />
+    <div className="flex flex-row text-xs justify-center items-center gap-1.5 font-light">
+      <Copyright size={16} strokeWidth={0.75} absoluteStrokeWidth />
       <p>Nooa Peltokangas</p>
     </div>
   )
