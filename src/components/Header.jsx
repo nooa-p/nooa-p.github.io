2988841d7664980a84f9aa6f8912cdb18fcfbd65
Nooa Peltokangas <nooa.peltokangas@protonmail.com>
2024-02-20 12:28:47 +0200

    fix link wrapping on smaller screens

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,8 @@
 function Header() {
   return (
-    <header className="flex flex-row justify-between lowercase md:text-lg bg-slate-50 px-6 py-4 rounded-md smooth-shadow">
+    <header className="flex flex-row justify-between items-center lowercase md:text-lg bg-slate-50 px-6 py-4 rounded-md smooth-shadow">
       <h1 className="hover:cursor-default font-extrabold">Nooa P.</h1>
-      <nav className="flex flex-row gap-x-4 md:gap-x-8 font-semibold">
+      <nav className="flex flex-row gap-x-2 md:gap-x-8 font-semibold text-sm md:text-lg">
         <a href="#about">About</a>
         <a href="#project">Projects</a>
         <a href="#contact">Contact</a>
