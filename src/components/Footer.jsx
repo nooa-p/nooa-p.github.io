f13ab39fed226929c63c43b9331622f38a1ff04e
nooa-p <nooa.peltokangas@protonmail.com>
2024-03-14 13:04:01 +0200

    intro component done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <div className="flex flex-row text-xs justify-center items-center gap-1.5 font-light">
       <Copyright size={14} strokeWidth={0.75} absoluteStrokeWidth />
-      <p>Nooa Peltokangas</p>
+      <p className="leading-3 -mt-[0.5px]">Nooa Peltokangas</p>
     </div>
   )
 }
--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,7 +1,13 @@
 function Intro() {
   return (
-    <div className="border-b">
-      <h2>Hi, I&apos;m Nooa</h2>
+    <div className="border-b px-12 py-12">
+      <div className="flex flex-row items-center pl-8 gap-6">
+        <div className="profile-outline">
+        <img src="profile.png" className="rounded-full w-20 profile-outset" />
+        </div>
+        <h2 className="text-3xl font-semibold">hi, i&apos;m nooa &#128075;</h2>
+      </div>
+      <h3 className="text-xl font-light pt-4 px-4 leading-9">a <span className="title-underline font-medium">full-stack web developer</span>, with special interest in creating simple yet aesthetic web design and practical data tracking tools</h3>
     </div>
   )
 }
