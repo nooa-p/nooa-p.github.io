7231d951c20404d93669c4b4aaf2658a082c6f16
nooa-p <nooa.peltokangas@protonmail.com>
2024-03-14 14:22:04 +0200

    fix intro on mobile

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -2,15 +2,15 @@ import { IconAt, IconBrandGithub, IconBrandLinkedin } from "@tabler/icons-react"
 
 function Intro() {
   return (
-    <div className="border-b px-12 py-12">
-      <div className="flex flex-row items-center pl-8 gap-6">
+    <div className="border-b p-3 pb-5 sm:p-12">
+      <div className="flex flex-row items-center sm:pl-8 gap-6">
         <div className="profile-outline">
         <img src="profile.png" className="rounded-full w-20 profile-outset" />
         </div>
         <h2 className="text-3xl font-semibold">hi, i&apos;m nooa &#128075;</h2>
       </div>
-      <h3 className="text-xl font-light pt-4 px-4 leading-9">a <span className="title-underline font-medium">full-stack web developer</span>, with special interest in creating simple yet aesthetic web design and practical data tracking tools</h3>
-      <div className="flex flex-row px-4 pt-1 gap-4 text-base font-normal text-slate-600 leading-4">
+      <h3 className="text-xl font-light pt-4 sm:px-4 leading-9">a <span className="title-underline font-medium">full-stack web developer</span>, with special interest in creating simple yet aesthetic web design and practical data tracking tools</h3>
+      <div className="flex flex-col sm:flex-row sm:px-4 pt-2 sm:pt-1 gap-4 text-base font-normal text-slate-600 leading-4">
         <div className="flex flex-row gap-1 items-center">
           <IconAt size={18} className="text-[#fab8b2]" />
           <a href="mailto:nooa@peltokangas.dev" className="">mail</a>
