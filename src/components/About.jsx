3f5fdd46e5b684973a956f4e39f70ea1ba5ba241
nooa-p <nooa.peltokangas@protonmail.com>
2024-03-15 09:46:09 +0200

    finish styling skill boxes, add all skills

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,19 +1,28 @@
-import { IconBrandJavascript, IconBrandTypescript, IconBrandReact } from "@tabler/icons-react"
+import { IconBrandJavascript, IconBrandTypescript, IconBrandReact, IconBrandNextjs, IconBrandCss3, IconBrandTailwind, IconBrandHtml5, IconBrandNodejs, IconBrandPython, IconBrandGit, IconBrandMongodb, IconSql } from "@tabler/icons-react"
 
 function Skills() {
   const skills = [
     {icon: IconBrandJavascript, label: 'JavaScript'},
     {icon: IconBrandTypescript, label: 'TypeScript'},
-    {icon: IconBrandReact, label: 'React'}
+    {icon: IconBrandReact, label: 'React'},
+    {icon: IconBrandNextjs, label: 'Next.js'},
+    {icon: IconBrandCss3, label: 'CSS3'},
+    {icon: IconBrandTailwind, label: 'Tailwind'},
+    {icon: IconBrandHtml5, label: 'HTML5'},
+    {icon: IconBrandNodejs, label: 'Node.js'},
+    {icon: IconBrandMongodb, label: 'MongoDB'},
+    {icon: IconSql, label: 'PostgreSQL'},
+    {icon: IconBrandGit, label: 'Git'},
+    {icon: IconBrandPython, label: 'Python'}
   ]
 
   return (
     skills.map((skill) => {
       return (
-        <div key={skill.label} className="w-1/2 p-1">
-          <div className="flex flex-col bg-romantic-50">
-          <skill.icon />
-          <h4>{skill.label}</h4>
+        <div key={skill.label} className="w-1/2 p-0.5">
+          <div className="flex flex-col bg-romantic-100 rounded items-center p-2.5 gap-0.5 border border-romantic-200">
+          <skill.icon size={40} strokeWidth={1} />
+          <h4 className="uppercase text-[11px]">{skill.label}</h4>
           </div>
         </div>
       )
@@ -25,7 +34,7 @@ function About() {
   return (
     <div id="about" className="border-b flex flex-col sm:flex-row py-4">
       <div className="basis-9/12">
-        <h3 className="text-lg font-medium">about</h3>
+        <h3 className="text-lg font-medium">about me</h3>
       </div>
       <div className="basis-3/12">
         <h3 className="text-lg font-medium">skills</h3>
